617f22cd4845a3300a9567b011fea2e6a0679202
menuHwang <hmin0206@naver.com>
2023-01-10 17:49:08 +0900

    [Feat] hello2 구현

--- src/main/java/com/example/gitaction/HelloController.java
+++ src/main/java/com/example/gitaction/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/2")
+    public String hello2() {
+        return "hello2";
+    }
 }
